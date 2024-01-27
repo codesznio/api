50782624880385a31d396eecf9517a0cda62e140
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-01-27 19:37:47 +0400

    feat: add profile controller and repo

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
-// Schema
+// Controllers
+import { ProfileController } from './profile.controller'
+
+// Services
+import { ProfileService } from './profile.service'
 import { Profile, ProfileSchema } from './profile.schema'
+
+// Repository
+import { ProfileRepository } from './profile.repository'
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: Profile.name, schema: ProfileSchema }])],
-    controllers: [],
-    providers: [],
-    exports: [],
+    controllers: [ProfileController],
+    providers: [ProfileRepository, ProfileService],
+    exports: [ProfileService],
 })
 export class ProfileModule {}
