b8b8ee22c394c367c15d60b40e702b0744d926fb
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-01-27 20:09:17 +0400

    feat: update activity module

--- src/modules/activity/activity.module.ts
+++ src/modules/activity/activity.module.ts
@@ -3,11 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose'
 
 // Activity
 import { Activity, ActivitySchema } from './activity.schema'
+import { ActivityRepository } from './activity.repository'
+
+// Controller
+import { ActivityController } from './activity.controller'
+
+// Service
+import { ActivityService } from './activity.service'
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: Activity.name, schema: ActivitySchema }])],
-    controllers: [],
-    providers: [],
-    exports: [],
+    controllers: [ActivityController],
+    providers: [ActivityRepository, ActivityService],
+    exports: [ActivityService],
 })
 export class ActivityModule {}
