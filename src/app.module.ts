b1265a8840c0d73612c729d99a02398e05999a30
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-01-27 20:29:23 +0400

    feat: permission module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { ProvidersModule } from '@/providers'
 // Modules
 import { ActivityModule } from '@/modules/activity/activity.module'
 import { AuthenticationModule } from '@/modules/authentication/authentication.module'
+import { PermissionModule } from '@/modules/permission/permission.module'
 import { ProfileModule } from '@/modules/profile/profile.module'
 import { UserModule } from '@/modules/user/user.module'
 
@@ -26,6 +27,7 @@ import { MailerModule } from '@/utils/mailer/mailer.module'
         // Modules
         ActivityModule,
         AuthenticationModule,
+        PermissionModule,
         ProfileModule,
         UserModule,
 
