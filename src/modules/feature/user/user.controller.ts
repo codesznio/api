8bdb7498826b22017ddc3b3f8c00220d298eaa62
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-01-29 16:56:36 +0400

    feat: sanitize data

--- src/modules/feature/user/user.controller.ts
+++ src/modules/feature/user/user.controller.ts
@@ -13,14 +13,18 @@ import { GetPayload } from '@/shared/decorators/get-payload.decorator'
 // Types
 import { Api } from '@/data/types/api'
 
-@Controller('users')
+@Controller('user')
 export class UserController {
     constructor(private _profileService: UserService) {}
 
     @UseGuards(AuthorizationGuard)
     @Get('me')
-    async me(@GetPayload() payload: Api.JwtPayload): Promise<Api.Response<User>> {
+    async me(@GetPayload() payload: Api.JwtPayload): Promise<Api.Response<Partial<User>>> {
         const data = await this._profileService.retrieve.byId(payload.user)
+
+        // Sanitize sensative data
+        data.tokens = null
+        data.credentials.password = null
 
         return {
             data,
