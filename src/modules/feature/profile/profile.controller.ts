4f67adc508aea244b3b86cb1c3bbd695199c0108
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-01-29 16:57:03 +0400

    Get Me (#9)
    * feat: me
    
    * feat: sanitize data

--- src/modules/feature/profile/profile.controller.ts
+++ src/modules/feature/profile/profile.controller.ts
@@ -1,11 +1,29 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, UseGuards } from '@nestjs/common'
 
-@Controller('profiles')
+// Data
+import { Profile } from './profile.schema'
+
+// Profile Service
+import { ProfileService } from './profile.service'
+
+// Shareds
+import { AuthorizationGuard } from '@/shared/guards/authorization.guard'
+import { GetPayload } from '@/shared/decorators/get-payload.decorator'
+
+// Types
+import { Api } from '@/data/types/api'
+
+@Controller('profile')
 export class ProfileController {
-    @Get('')
-    async test() {
+    constructor(private _profileService: ProfileService) {}
+
+    @UseGuards(AuthorizationGuard)
+    @Get('me')
+    async me(@GetPayload() payload: Api.JwtPayload): Promise<Api.Response<Profile>> {
+        const data = await this._profileService.retrieve.byUser(payload.user)
+
         return {
-            data: 'profile endpoint',
+            data,
             success: true,
         }
     }
